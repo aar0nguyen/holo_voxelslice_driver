be42e4f2c525da0a71761c55f2c89c38e4bc60e4
Aaron Nguyen <aaronn78566@gmail.com>
2025-02-04 20:52:29 -0600

    Camera Slicing
    Uses camera as image slicer
    Creates image slice at 64x64
    
    Need To Do
    - Need to save render texture as png or bitmap

---
+++ voxelslice_driver/Assets/Scripts/RotateCamera.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateCamera : MonoBehaviour
+{
+    public float speed;
+
+
+    private void Update()
+    {
+        transform.Rotate(0, speed * Time.deltaTime, 0);
+    }
+}
