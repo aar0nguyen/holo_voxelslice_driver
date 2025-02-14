e74a646d498f73b8ce698efaf6b0fcd5f1ed32f3
Aaron Nguyen <aaronn78566@gmail.com>
2025-02-14 00:37:36 -0600

    Change to RPM rotation speed

--- voxelslice_driver/Assets/Scripts/rotate_camera.cs
+++ voxelslice_driver/Assets/Scripts/rotate_camera.cs
@@ -4,11 +4,11 @@ using UnityEngine;
 
 public class rotate_camera : MonoBehaviour
 {
-    public float speed;
+    public float rpm;
 
 
     private void Update()
     {
-        transform.Rotate(0, speed * Time.deltaTime, 0);
+        transform.Rotate(0, rpm * 6f * Time.deltaTime, 0);
     }
 }
