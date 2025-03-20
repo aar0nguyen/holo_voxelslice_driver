a0f81f12666482da407005c92c338f9d3428ec0d
Aaron Nguyen <aaronn78566@gmail.com>
2025-03-20 11:52:33 -0500

    Init Animation Controller script
    TODO: Need to collab with AI Team to get animation state from API calls

---
+++ voxelslice_driver/Assets/Scripts/AnimationController.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.Animations;
+using System.Collections;
+
+public class AnimationController : MonoBehaviour
+{
+
+    public Animator animator;
+
+    private void Start()
+    {
+        
+    }
+
+    public void changeAnimationState(string state)
+    {
+        switch (state)
+        {
+            // model thinking 
+            case "Thinking":
+                break;
+
+            // model talking
+            case "Talking":
+                break;
+
+            // idle state
+            default:
+                break;
+
+        }
+
+    }
+
+}
